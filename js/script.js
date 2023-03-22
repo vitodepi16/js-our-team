6ca3b404b5d413497d3996983be7e06d6b89e681
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-03-22 15:08:37 +0100

    add elements in array js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const team = [
+    {
+        username : 'Wayne',
+        surname: 'Barnett',
+        role: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        username : 'Angela',
+        surname: 'Caroll',
+        role: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        username : 'Angela',
+        surname: 'Lopez',
+        role: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        username : 'Scott',
+        surname: 'Estrada',
+        role: 'Developer',
+        img: 'scott-estrada-developer.jpg'
+    },
+    {
+        username : 'Barbara',
+        surname: 'Ramos',
+        role: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg'
+    },
+
+]
+
+
+
